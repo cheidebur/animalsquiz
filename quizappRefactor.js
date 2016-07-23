0f2626277f02017781aec20a6a06af3c2fad37d8
cheidebur <spekter.x@gmail.com>
2016-07-23 04:17:39 -0700

    fixed score tracker and warning dialog bug

--- quizappRefactor.js
+++ quizappRefactor.js
@@ -1,24 +1,17 @@
-
 var Question = function(options, correctAnswer, question) {
-	this.options = options,
-	this.correctAnswer = correctAnswer,
-	this.question = question
+    this.options = options,
+        this.correctAnswer = correctAnswer,
+        this.question = question
 }
 
 var Answer = function(answer) {
-	this.correct = answer
+    this.correct = answer
 }
 
-var CorrectAnswers = null
-
 var totalQuestions = 7
-
-var num = 1
-
 var correctScore = 0
-
+//answer is the input value from the bottoms at the bottom of the script
 var answer = 1
-
 
 
 var answer1 = new Answer(3)
@@ -42,165 +35,133 @@ var question6 = new Question(["Gorilla", "Bonobo", "Lion", "Sea Lion"], answer6.
 var answer7 = new Answer(2)
 var question7 = new Question(["Mouse", "Fly", "Frog", "Alpaca"], answer7.correct, "Which animal can taste with its feet?")
 
-
+//the Quiz objects takes an array of objects which is the following
 var questions = [question1, question2, question3, question4, question5, question6, question7];
 
 
 
 var Quiz = function(questions) {
-this.n = questions.length;
-this.currentIdx = 0;
-this.displayQuestion = function() {
-input = false; // reset input validation
-
-		if ( this.currentIdx < this.n) { 
-		
-		$("#qRemainingBox").text((this.n) - (this.currentIdx) + " questions left.");
-		$("#qSection").text(questions[this.currentIdx].question).css('visibility', 'visible').hide().fadeIn(400);
-		$("#option1").text(questions[this.currentIdx].options[0]).hide().fadeIn(750);
-		$("#option2").text(questions[this.currentIdx].options[1]).hide().fadeIn(850);
-		$("#option3").text(questions[this.currentIdx].options[2]).hide().fadeIn(950);
-		$("#option4").text(questions[this.currentIdx].options[3]).hide().fadeIn(1050);
-		$("#next").css('visibility', 'visible').hide().fadeIn(1200);
-		$("#qNumbaBox").html("question " + (this.currentIdx + 1));
-		console.log("Your function was executed.");
-
-		} else { //displays results and play again button
-
-
-			this.resetGame();
-		};
-	}
+    this.n = questions.length;
+    this.currentIdx = 0;
+    this.displayQuestion = function() {
+        // reset input validation
+        input = false;
+        $("#qRemainingBox").fadeIn();
+    	$("#qNumbaBox").fadeIn();
+        if (this.currentIdx < this.n) {
+            $("#qRemainingBox").text((this.n) - (this.currentIdx) + " questions left.");
+            $("#qSection").text(questions[this.currentIdx].question).css('visibility', 'visible').hide().fadeIn(400);
+            $("#option1").text(questions[this.currentIdx].options[0]).hide().fadeIn(750);
+            $("#option2").text(questions[this.currentIdx].options[1]).hide().fadeIn(850);
+            $("#option3").text(questions[this.currentIdx].options[2]).hide().fadeIn(950);
+            $("#option4").text(questions[this.currentIdx].options[3]).hide().fadeIn(1050);
+            $("#next").css('visibility', 'visible').hide().fadeIn(1200);
+            $("#qNumbaBox").html("question " + (this.currentIdx + 1));
+        } else { //displays results and play again button
+            this.resetGame();
+        };
+    }
 }
 
-Quiz.prototype.resetGame = function() {
-	$("#qRemainingBox").fadeOut();
-	$("#qNumbaBox").fadeOut();
 
-	console.log("holler")
-	this.currentIdx = 0;
-	$(".oButton").hide();
-	$("#qSection").html("Giddy goat! You got " + correctScore + " of " + totalQuestions + " total questions correct!").css('visibility', 'visible').hide().fadeIn(400);
-	$("#playAgain").css('display', 'block').hide().fadeIn(1200);
-	$("#playAgain").click(function () {
-		$("#playAgain").css('display', 'none');
-		quiz.displayQuestion();
-	});
+//resetGame is actually the final screen that displays the score
+Quiz.prototype.resetGame = function() {
+    $("#qRemainingBox").fadeOut();
+    $("#qNumbaBox").fadeOut();
+    this.currentIdx = 0;
+    $(".oButton").hide();
+    $("#qSection").html("Giddy goat! You got " + correctScore + " of " + totalQuestions + " total questions correct!").css('visibility', 'visible').hide().fadeIn(400);
+    $("#playAgain").css('display', 'block').hide().fadeIn(1200);
+    $("#playAgain").click(function() {
+        $("#playAgain").css('display', 'none');
+        quiz.displayQuestion();
+    });
 }
 
 
 Quiz.prototype.currentquestion = function() {
-	this.questions[this.currentIdx]
+    this.questions[this.currentIdx]
 }
 
 Quiz.prototype.nextQuestion = function() {
-	this.currentIdx += 1;
+    this.currentIdx += 1;
 }
-
-
 
 Quiz.prototype.newGame = function() {
-	
-	num = 1;
-	correctScore = 0;
-	answerCorrect = 0;
-	$("#playAgain").click(this.newGame())
-	$("#playAgain").css('visibility', 'hidden');
-	$("#qSection").css('visibility', 'visible').hide();
-	$("#introDialogue").css('display', 'block');
+    correctScore = 0;
+    $("#playAgain").click(this.newGame())
+    $("#playAgain").css('visibility', 'hidden');
+    $("#qSection").css('visibility', 'visible').hide();
+    $("#introDialogue").css('display', 'block');
 }
 
-
-
-Quiz.prototype.answerAdd = function () {
-
-	if (answer == (questions[this.currentIdx].correctAnswer)) {
-		++correctScore;
-	}
-	this.nextQuestion();
-	this.displayQuestion();
-
+//answerAdd adds the correct answer to the score, but only
+//if the input was validated by answerCheck
+Quiz.prototype.answerAdd = function() {
+    if (answer == (questions[this.currentIdx].correctAnswer)) {
+        ++correctScore;
+    }
+    this.nextQuestion();
+    this.displayQuestion();
 };
 
 Quiz.prototype.answerCheck = function() {
-	$("#warningDialogue").css('visibility', 'hidden') // hide hold ur horses dialogue
-	if (input == true)  {
-	
-	$(".oButton").css('background-color', '');
-	this.answerAdd();
-	
-	} else { // show warning dialogue if no answer selected
-		$("#warningDialogue").css('visibility', 'visible') 
-		$("#warningDialogue").html("Hold your horses! Please select an option.").css('visibility', 'visible').hide().fadeIn(400);
-	};
-
+    $("#warningDialogue").css('visibility', 'hidden') // hide hold ur horses dialogue
+    if (input == true) {
+        $(".oButton").css('background-color', '');
+        this.answerAdd();
+    } else { // show warning dialogue if no answer selected
+        $("#warningDialogue").css('visibility', 'visible')
+        $("#warningDialogue").html("Hold your horses! Please select an option.").css('visibility', 'visible').hide().fadeIn(400);
+    };
 }
-
-
 
 var quiz = new Quiz(questions);
 
-
-
-
-
-
-
-$("button").click(function(){
-	quiz.displayQuestion();
-	$("#introDialogue").css('display', 'none');
+$("button").click(function() {
+    quiz.displayQuestion();
+    $("#introDialogue").css('display', 'none');
 });
 
 
+//Since I didn't use radio buttons, this code assigns a value to each of
+//the buttons
 
-//\/\/\/ log values from buttons and provide feedback responsiveness \/\/\/
+$("#option1").click(function() {
+    answer = 1;
+    //this resets the background color of all buttons in case
+    //a user changes their choice and chooses a different button
+    $(".oButton").css('background-color', '');
+    //changes the selected element's background color
+    $(this).css('background-color', 'aquamarine');
+    //validates input
+    input = true;
+})
 
+$("#option2").click(function() {
+    answer = 2;
+    $(".oButton").css('background-color', '');
+    $(this).css('background-color', 'aquamarine');
+    input = true;
+})
 
-	$("#option1").click(function() {
-		answer = 1;
-		$(".oButton").css('background-color', '');
-		$(this).css('background-color', 'aquamarine');
-		input = true;
-	})
+$("#option3").click(function() {
+    answer = 3;
+    $(".oButton").css('background-color', '');
+    $(this).css('background-color', 'aquamarine');
+    input = true;
+})
 
-	$("#option2").click(function() {
-		answer = 2;
-		$(".oButton").css('background-color', '');
-		$(this).css('background-color', 'aquamarine');
-		input = true;
-	})
-
-	$("#option3").click(function() {
-		answer = 3;
-		$(".oButton").css('background-color', '');
-		$(this).css('background-color', 'aquamarine');
-		input = true;
-	})
-
-	$("#option4").click(function() {
-		answer = 4;
-		$(".oButton").css('background-color', '');
-		$(this).css('background-color', 'aquamarine');
-		input = true;
-
-
-	})
-
-	$("#next").click(function() {
-		quiz.answerCheck();
-
-	})
+$("#option4").click(function() {
+    answer = 4;
+    $(".oButton").css('background-color', '');
+    $(this).css('background-color', 'aquamarine');
+    input = true;
 
 
-//\/\/\/ check if answer was chosen and display dialogue if not \/\/\/
+})
 
-
-
-
-
-
-//\/\/\/ log correct answer and proceed to next question \/\/\/
-
-
-
-
+//answerCheck is the input validation function
+$("#next").click(function() {
+    quiz.answerCheck();
+})
